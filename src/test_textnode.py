79ff682d7557243b86c53cc0a0f1e37b3ae161a5
tebix <esteban.torres.perez@outlook.com>
2025-05-14 22:08:17 -0700

    import testing module and create some test cases.

---
+++ src/test_textnode.py
@@ -0,0 +1,14 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a different text node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+
+if __name__ == "__main__":
+    unittest.main()
